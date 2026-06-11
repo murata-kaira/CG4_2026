1451e57ff86f398ab5e9f4222ed3fd72b68e4e33
murata-kaira <k024g0062@g.neec.ac.jp>
2026-06-11 16:14:24 +0900

    hihihihi

--- DirectXGame/Model2.cpp
+++ DirectXGame/Model2.cpp
@@ -830,21 +830,21 @@ Model2* Model2::CreateSquare(int max) {
 
 	for (int i = 0; i < max; i++) {
 		int index = i * 4;
-		// 左下
-		vertices[index + 0].pos = {i * 2 + -1.0f, -1.0f, 0.0f};
+		float offsetX = i * 2.0f; // これが横にずらす量
+
+		vertices[index + 0].pos = {-1.0f + offsetX, -1.0f, 0.0f}; // 左下
+		vertices[index + 1].pos = {-1.0f + offsetX, 1.0f, 0.0f};  // 左上
+		vertices[index + 2].pos = {1.0f + offsetX, -1.0f, 0.0f};  // 右下
+		vertices[index + 3].pos = {1.0f + offsetX, 1.0f, 0.0f};   // 右上
+
 		vertices[index + 0].uv = {0, 1};
-		vertices[index + 0].normal = {0, 0, 1};
-		// 左上
-		vertices[index + 1].pos = {i * 2 + -2.0f, 1.0f, 0.0f};
 		vertices[index + 1].uv = {0, 0};
-		vertices[index + 1].normal = {0, 0, 1};
-		// 右下
-		vertices[index + 2].pos = {i * 2 + 1.0f, -1.0f, 0.0f};
 		vertices[index + 2].uv = {1, 1};
-		vertices[index + 2].normal = {0, 0, 1};
-		// 右上
-		vertices[index + 3].pos = {i * 2 + 2.0f, 1.0f, 0.0f};
 		vertices[index + 3].uv = {1, 0};
+
+		vertices[index + 0].normal = {0, 0, 1};
+		vertices[index + 1].normal = {0, 0, 1};
+		vertices[index + 2].normal = {0, 0, 1};
 		vertices[index + 3].normal = {0, 0, 1};
 	}
 
